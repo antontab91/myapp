3d5341f945d3b91eb1bf51463b178531e229c04d
Milkweedd <antontab91@gmail.com>
2020-05-20 14:37:44 +0300

    ыф

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ console.log(rootElement);
 
 const element = (
   <div className="greeting">
-    <div className="greeting__title">Hello, World!</div>
+    <div className="greeting__title">Hello, world!</div>
     <div className="greeting__text">I'm learning React</div>
   </div>
 );
